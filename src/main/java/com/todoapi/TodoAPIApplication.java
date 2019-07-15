02f23261d64b4c8ea987e5f571a897341b45da31
ttjb2 <tom.bauer95@gmail.com>
2019-07-15 10:28:45 -0500

    added spring-boot, able to run

---
+++ src/main/java/com/todoapi/TodoAPIApplication.java
@@ -0,0 +1,13 @@
+package com.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAPIApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TodoAPIApplication.class, args);
+    }
+
+}
+
